aa129fadf7226766dd565035362859bb156f668d
alvina1104 <albina9198@icloud.com>
2026-08-11 16:07:27 +0600

    Save local project changes

--- main.py
+++ main.py
@@ -134,4 +134,4 @@ async def gtzan_audio(file: UploadFile = File(...)):
 
 
 if __name__ == '__main__':
-    uvicorn.run(gtzan_app, host="127.0.0.1", port=8000)
+    uvicorn.run(gtzan_app, host="127.0.0.1", port=8001)
